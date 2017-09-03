3f3429067fdb968e3068af8e99bc817755278b99
Carlos Samouco <carlosamouco@gmail.com>
2017-09-03 16:14:17 +0000

    script improve

--- server/src/python/scripts/weather.py
+++ server/src/python/scripts/weather.py
@@ -1,20 +1,41 @@
 """This script gathers data from the sense hat."""
 from sense_hat import SenseHat
 from datetime import datetime
+from time import sleep
 import json
 
 sense = SenseHat()
 
-TEMPERATURE = sense.get_temperature_from_humidity()
-# TEMPERATURE2 = sense.get_temperature_from_pressure()
-PRESSURE = sense.get_pressure()
-HUMIDITY = sense.get_humidity()
+tempList = []
+# temp2List = []
+presList = []
+humList = []
 
-TEMPERATURE = round(TEMPERATURE, 2)
-# TEMPERATURE2 = round(TEMPERATURE2, 2)
-PRESSURE = round(PRESSURE, 2)
-HUMIDITY = round(HUMIDITY, 2)
+for i in range(0, 6):
+    TEMPERATURE = sense.get_temperature_from_humidity()
+    # TEMPERATURE2 = sense.get_temperature_from_pressure()
+    PRESSURE = sense.get_pressure()
+    HUMIDITY = sense.get_humidity()
 
+    TEMPERATURE = round(TEMPERATURE, 2)
+    # TEMPERATURE2 = round(TEMPERATURE2, 2)
+    PRESSURE = round(PRESSURE, 2)
+    HUMIDITY = round(HUMIDITY, 2)
+    
+    tempList.append(TEMPERATURE)
+    # temp2List.append(TEMPERATURE2)
+    presList.append(PRESSURE)
+    humList.append(HUMIDITY)
+    
+    sleep(0.05)
+
+
+TEMPERATURE = round(sum(tempList) / len(tempList), 2)
+# TEMPERATURE2 = round(sum(temp2List) / len(temp2List), 2)
+PRESSURE = round(sum(presList) / len(presList), 2)
+HUMIDITY = round(sum(humList) / len(humList), 2)
+
+# print(TEMPERATURE2)
 
 JSON = {"temperature": TEMPERATURE, "humidity": HUMIDITY, "pressure": PRESSURE, "creation_date": datetime.now().isoformat()}
 
