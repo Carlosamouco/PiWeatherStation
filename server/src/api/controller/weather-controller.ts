6550405f5c635702a04ccdf68833fb84b14579dc
Carlosamouco <carlosamouco@hotmail.com>
2017-09-09 21:21:09 +0100

    removed cron job and added routes

--- server/src/api/controller/weather-controller.ts
+++ server/src/api/controller/weather-controller.ts
@@ -8,20 +8,26 @@ export class WeatherController {
       .catch(error => res.status(400).json(error));
   }
 
-  static addMeasure(req: express.Request, res: express.Response): void {
-    WeatherHistory.addMeasure(req.body)
-      .then(result => res.status(200).json(result.rows))
-      .catch(error => res.status(400).json(error));
-  }
-
   static getByDate(req: express.Request, res: express.Response):void {
     WeatherHistory.getByDate(req.params.start, req.params.end)
       .then(result => res.status(200).json(result.rows))
       .catch(error => res.status(400).json(error));
   }
 
-  static getSummary(req: express.Request, res: express.Response):void {
-    WeatherHistory.getSummary()
+  static addMeasure(req: express.Request, res: express.Response): void {
+    WeatherHistory.addMeasure(req.body)
+      .then(result => res.status(200).json(result.rows))
+      .catch(error => res.status(400).json(error));
+  }
+
+  static getDailySummary(req: express.Request, res: express.Response):void {
+    WeatherHistory.getDailySummary(req.params.start, req.params.end)
+      .then(result => res.status(200).json(result.rows))
+      .catch(error => res.status(400).json(error));
+  }
+
+  static getDetailedSummary(req: express.Request, res: express.Response):void {
+    WeatherHistory.getDetailedSummary(req.params.interval, req.params.start, req.params.end)
       .then(result => res.status(200).json(result.rows))
       .catch(error => res.status(400).json(error));
   }
