7d5c8732478b2d247b7d77484f9ed5cdb5c8f12a
Carlos Samouco <carlossamouco@criticalmanufacturing.com>
2026-01-01 04:13:23 +0000

    rename

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
-# Set the variables for this session
 export ARCH_DB="arm64v8/postgres:18"
 export ARCH_NGINX="arm64v8/nginx:1.29-alpine"
 export ARCH_NODE="arm64v8/node:20-alpine"
 export ARCH_PGADMIN="benuhx/pgadmin4-pi"
 export BUILD_TARGET="prod"
 
-# Run compose
 docker compose -f docker-compose.yml up -d --build
